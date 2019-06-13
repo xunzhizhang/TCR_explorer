8ee21f54fe4d37c719b9e200fa4be19b9562607e
zhanxw <zhanxw@gmail.com>
2019-06-13 04:33:23 -0500

    order colors according to antigen frequency

--- TCRexplorer.R
+++ TCRexplorer.R
@@ -2,6 +2,7 @@ library(ggplot2)
 library(Rcpp)
 # library(cairo) for better fig in Linux
 library(shiny)
+options(stringsAsFactors = FALSE)
 
 ui <- fluidPage(
   tags$h1("TCR Sequences",
@@ -15,7 +16,8 @@ ui <- fluidPage(
             multiple=FALSE,
             accept=c(".csv")
   ),
-  downloadButton('download_file', 'Download analyzed results'),
+  downloadButton('download_example', 'Download example raw sequences'),
+  downloadButton('download_result', 'Download analyzed results'),
   
   # Interactive plot
   tags$h4("Brush to zoom the target region"),
@@ -73,6 +75,7 @@ server <- function(input, output) {
                      linestream >> val;
                      BLOSUM62[(unsigned char) a1[0]][(unsigned char) a2[0]] = val;  
                 }
+                return 0;
               }
             
             // [[Rcpp::export]]
@@ -169,6 +172,16 @@ server <- function(input, output) {
   # Initilize ranges for axis
   ranges <- reactiveValues(rx = NULL, ry = NULL)
   ranges_z <- reactiveValues(rzx = NULL, rzy = NULL)
+  # function to get first 500 rows of raw seq
+  len.test <- function(raw_seq) {
+    l <- length(raw_seq)
+    if (l >500){
+      print("Warning: > 500 rows detected! Only fist 500 rows will be analyzed.")
+      return(head(raw_seq, 500))
+    } else {
+      return(raw_seq)
+    }
+  }
   # Read demo data/user data/TCR seq
   # Read the file as a data frame, 1st line as header, return empty data frame without input
   # Note: a .csv file with only seq column will be treated to generate entire file
@@ -183,7 +196,7 @@ server <- function(input, output) {
         )
       )
     } else if (is.null(docu)) {
-      empdata<-data.frame(tcr=c(NA),x=c(NA),y=c(NA),color=c(NA))
+      empdata<-data.frame(tcr=c(NA),x=c(NA),y=c(NA),color=c(NA)) # color is required here
       return(empdata)
     } else if (slc == "u") {
       return(read.csv(
@@ -193,38 +206,30 @@ server <- function(input, output) {
         stringsAsFactors = FALSE
       ))
     } else {
+      results <- data.frame(tcr=c(NA),x=c(NA),y=c(NA),color=c(NA))
       tcr_seq <- read.csv(
         docu$datapath,
         header = TRUE,
         sep = ",",
         stringsAsFactors = FALSE
       )[, 1] # read a data frame with mere seq, get a char vector WITHOUT title
-      # print(tcr_seq)
-      # print(tcr_seq[1])
-      # print(BLOSUM62)
       # analyze with C++, get a matrix and then transfer to dataframe
-      slen <- length(tcr_seq)   # cal len of data in R
-      # results <- r_result(tcr_seq, slen)
-      # get dis matrix and then transfer to dataframe with PEA
-      # sourceCpp("maintest.cpp")
-      #dist_matrix <- as.matrix(dist(loc_m(tcr_seq, slen)))
+      # Only first 500 rows are treated
+      tcr_seq <- len.test(tcr_seq)
+      slen <- length(tcr_seq) 
       dist_matrix <- loc_m(tcr_seq, slen)
       save(dist_matrix, file = "saved.dist_matrix.Rdata")
       # print(dist_matrix[1:5, 1:5])
       # print(tcr_seq)
       # print(slen)
-      # save.image(file = "saved.Rdata")
       h <- diag(slen) - matrix(1, slen, 1) %*% matrix(1, 1, slen) / slen
       ret <- eigen(h %*% (-0.5 * dist_matrix) %*% h, symmetric = TRUE)
       x <- ret$vectors [, 1:2] %*% diag(ret$values[1:2])[,1]
       y <- ret$vectors [, 1:2] %*% diag(ret$values[1:2])[,2]
       color <- colv(tcr_seq, slen)
+      save(color, file = "color.Rdata")
       # save(tcr_seq, file = "seq.Rdata")
       results <- as.data.frame(cbind(tcr_seq,x,y,color), stringsAsFactors = FALSE)
-      # print(results$x)
-      # results$x <- as.numeric(as.character(results$x))
-      # results$y <- as.numeric(as.character(results$y))
-      # print(results$x)
       # save(results, file = "matrix.Rdata")
       names(results) <- c("tcr","x","y","color")
       results$tcr <- as.character(results$tcr)
@@ -242,24 +247,26 @@ server <- function(input, output) {
     sort_freq <- sort(table(frame$color), decreasing = T, na.last = NA)
     # avoid transfering to int with one cluster
     sort_freq <- as.table(sort_freq)
-    # save(sort_freq, file = "5.Rdata")
+    save(sort_freq, file = "5.Rdata")
     # NA will be generated in table without color, which cannot be directly converted
     if (is.na(sort_freq[1])){
-      sort_frame <- data.frame(color=c(NA),frequency=c(NA))
+      sort_frame <- data.frame(color=c(NA),frequency=c(NA), plot_color=c("#ffffff"))
     } else {
       sort_frame <- as.data.frame(sort_freq, stringsAsFactors = FALSE)
+      sort_frame$color <- rainbow(nrow(sort_frame))
     }
-    names(sort_frame) <- c("color", "frequency")
+    names(sort_frame) <- c("color", "frequency", "plot_color")
     # save(sort_frame, file = "4.Rdata")
     # match and add freq after the original data
     r <- data.frame(tcr=frame$tcr, 
                     x=frame$x, 
                     y=frame$y, 
                     color=frame$color,
-                    frequency=sort_frame$frequency[match(frame$color, sort_frame$color)]
+                    frequency=sort_frame$frequency[match(frame$color, sort_frame$color)],
+                    plot_color=sort_frame$plot_color[match(frame$color, sort_frame$color)]
                     )
-    names(r) <- c("tcr","x","y","color","frequency")
-    # save(r, file = "3.Rdata")
+    names(r) <- c("tcr","x","y","color","frequency","plot_color")
+    save(r, file = "3.Rdata")
     return(r)
   }
   
@@ -283,9 +290,9 @@ server <- function(input, output) {
   output$plot_tcr <- renderPlot({
     ggplot(
       color.dist(safe.read(input$tcr_file, input$select_data)),
-      aes(x = x, y = y, color = color)) + geom_point() +
-      scale_color_manual(values = rainbow(
-        length(table(safe.read(input$tcr_file, input$select_data)$color)))) +
+      aes(x = x, y = y, color = color)) + 
+      geom_point(aes(color=color)) +
+      scale_color_manual(values = color.dist(safe.read(input$tcr_file, input$select_data))$plot_color) +
       coord_cartesian(xlim = ranges$rx, ylim = ranges$ry, expand = TRUE) +
       ggtitle("Entire data")
   })
@@ -295,41 +302,55 @@ server <- function(input, output) {
                input$plot_click,
                xvar = "x",
                yvar = "y",
-               # threshold = 50,
                addDist = TRUE
     )
   })
   # Right plot for zooming
   output$plot_tcr_z <- renderPlot({
+    data <- color.dist(safe.read(input$tcr_file, input$select_data))
+    color_table <- data[, c("color", "frequency", "plot_color")]
+    color_table <- color_table[!duplicated(color_table),]
+    color_table <- color_table[order(-color_table$frequency),]
+    data$color <- factor(data$color, levels = color_table$color)  ## control the order of color in the figure legend
+    color_table_mapping <- color_table$plot_color
+    names(color_table_mapping) <- color_table$color
     ggplot(
-      color.dist(safe.read(input$tcr_file, input$select_data)),
-      aes(x = x, y = y, color = color)) + geom_point() +
-      scale_color_manual(values = rainbow(
-        length(table(safe.read(input$tcr_file, input$select_data)$color)))) +
-      coord_cartesian(xlim = ranges_z$rzx, ylim = ranges_z$rzy, expand = TRUE) +
-      ggtitle("Chosen region")
+      data,
+      aes(x = x, y = y)) + 
+      geom_point(aes(col=color)) +
+      scale_color_manual(values = color_table_mapping) +
+      # coord_cartesian(xlim = ranges_z$rzx, ylim = ranges_z$rzy, expand = TRUE) +
+      ggtitle("Chosen region") 
   })
   output$info_z <- renderPrint({
     nearPoints(safe.read(input$tcr_file, input$select_data),
                input$plot_z_click,
                xvar = "x",
                yvar = "y",
-               # threshold = 50,
                addDist = TRUE
     )
   })
-  
+  # Downloadable file for raw TCR seq, as an example
+  output$download_example <- downloadHandler(
+    filename = function() {
+      paste("TCR_seq_example", "csv", sep = ".")
+    },
+    content = function(p) {
+      file.copy("TCR_seq_example.csv", p)
+    },
+    contentType = "text/csv"
+  )
   # Generate downloadable file with data frame generated by C++ or from input
   # Row names should be included in the data frame
-  output$download_file <- downloadHandler(
+  output$download_result <- downloadHandler(
     filename = function() {
       paste("TCR_analysis-", Sys.Date(), ".csv", sep = "")
     },
     content = function(file) {
       write.csv(safe.read(input$tcr_file, input$select_data), file, row.names=FALSE)
-    }
+    },
+    contentType = "text/csv"
   )
 }
-
 
 shinyApp(ui = ui, server = server)
