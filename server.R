a924d0c3a177ceccf1cb80179b991d5f8d93c626
xunzhizhang <50931410+xunzhizhang@users.noreply.github.com>
2019-06-21 13:22:22 -0500

    Limit reads without new input

--- server.R
+++ server.R
@@ -5,6 +5,9 @@ library(Rtsne)
 # library(cairo) for better fig in Linux
 library(shiny)
 options(stringsAsFactors = FALSE)
+dirtyFlag <- TRUE ## marked if the computation is needed or not
+cacheResult <- list()
+
 function(input, output) {
   sourceCpp("TCRfunctions.cpp")
   # Initilize ranges for axis
@@ -57,28 +60,26 @@ function(input, output) {
       slen <- length(tcr_seq) 
       dist_matrix <- loc_m(tcr_seq, slen)
       # calculate the coordinates
-          if (slc_method == "p"){
-            h <- diag(slen) - matrix(1, slen, 1) %*% matrix(1, 1, slen) / slen
-            ret <- eigen(h %*% (-0.5 * dist_matrix) %*% h, symmetric = TRUE)
-            x <- ret$vectors [, 1:2] %*% diag(ret$values[1:2])[,1]
-            y <- ret$vectors [, 1:2] %*% diag(ret$values[1:2])[,2]
-            cat("PCA\n")
-          } else {
-            # at least >3*perplexity+1 rows are required
-            set.seed(1)
-            tsne <- Rtsne(
-                          dist_matrix,
-                          dims = 2,
-                          pca = TRUE,
-                          initial_dims = 50,
-                          perplexity = 30,
-                          max_iter = 1000,
-                          theta = 0.0
-                          )
-            x <- tsne$Y[,1]
-            y <- tsne$Y[,2]
-            cat("t-SNE\n")
-          }
+      if (slc_method == "p"){
+        h <- diag(slen) - matrix(1, slen, 1) %*% matrix(1, 1, slen) / slen
+        ret <- eigen(h %*% (-0.5 * dist_matrix) %*% h, symmetric = TRUE)
+        x <- ret$vectors [, 1:2] %*% diag(ret$values[1:2])[,1]
+        y <- ret$vectors [, 1:2] %*% diag(ret$values[1:2])[,2]
+        cat("PCA\n")
+      } else {
+        # at least >3*perplexity+1 rows are required
+        set.seed(1)
+        tsne <- Rtsne(
+          dist_matrix,
+          dims = 2,
+          perplexity = 30,
+          max_iter = 1000,
+          theta = 0.0
+        )
+        x <- tsne$Y[,1]
+        y <- tsne$Y[,2]
+        cat("t-SNE\n")
+      }
       color <- colv(tcr_seq, slen)
       results <- as.data.frame(cbind(tcr_seq,x,y,color), stringsAsFactors = FALSE)
       names(results) <- c("tcr","x","y","color")
@@ -126,19 +127,42 @@ function(input, output) {
       ranges_z$rzy <- NULL
     }
   })
+  observeEvent(input$select_data, { dirtyFlag <<- TRUE })
+  observeEvent(input$select_method, { dirtyFlag <<- TRUE })
+  observeEvent(input$tcr_file, { dirtyFlag <<- TRUE })
   
   # Construct plot with zooming function
+  docu <- function(){
+    # cat("Dirty flag = ", dirtyFlag, "\n")
+    if (dirtyFlag) {
+      res <- safe.read(input$tcr_file, input$select_data, input$select_method)
+      cacheResult <<- res
+      dirtyFlag <<- FALSE
+    } else {
+      # do nothing
+    }
+    return(cacheResult)
+  }
+  data <- function(){
+    color.dist(docu())
+  }
+  color_table <- function(){
+    data()[, c("color", "frequency", "plot_color")]
+  }
+  color_table1 <- function(){
+    color_table()[!duplicated(color_table()),]
+  }                                            # extract unduplicated data
+  color_table2 <- function(){
+    color_table1()[order(-color_table1()$frequency),]
+  } 
   # Left plot with entire data
   output$plot_tcr <- renderPlot({
-    data <- color.dist(safe.read(input$tcr_file, input$select_data, input$select_method))
-    color_table <- data[, c("color", "frequency", "plot_color")]
-    color_table <- color_table[!duplicated(color_table),]         ## extract unduplicated data
-    color_table <- color_table[order(-color_table$frequency),]
-    data$color <- factor(data$color, levels = color_table$color)  ## control the order of color in the figure legend
-    color_table_mapping <- color_table$plot_color
-    names(color_table_mapping) <- color_table$color
+    data2 <- data()
+    data2$color <- factor(data2$color, levels = color_table2()$color)  ## control the order of color in the figure legend
+    color_table_mapping <- color_table2()$plot_color
+    names(color_table_mapping) <- color_table2()$color
     ggplot(
-      data,
+      data2,
       aes(x = x, y = y, color = color)) + 
       geom_point(aes(color=color)) +
       scale_color_manual(values = color_table_mapping) +
@@ -147,7 +171,7 @@ function(input, output) {
   })
   
   output$info <- renderPrint({
-    nearPoints(safe.read(input$tcr_file, input$select_data, input$select_method),
+    nearPoints(docu(),
                input$plot_click,
                xvar = "x",
                yvar = "y",
@@ -156,15 +180,12 @@ function(input, output) {
   })
   # Right plot for zooming
   output$plot_tcr_z <- renderPlot({
-    data <- color.dist(safe.read(input$tcr_file, input$select_data, input$select_method))
-    color_table <- data[, c("color", "frequency", "plot_color")]
-    color_table <- color_table[!duplicated(color_table),]         ## extract unduplicated data
-    color_table <- color_table[order(-color_table$frequency),]
-    data$color <- factor(data$color, levels = color_table$color)  ## control the order of color in the figure legend
-    color_table_mapping <- color_table$plot_color
-    names(color_table_mapping) <- color_table$color
+    data2 <- data()
+    data2$color <- factor(data2$color, levels = color_table2()$color)  ## control the order of color in the figure legend
+    color_table_mapping <- color_table2()$plot_color
+    names(color_table_mapping) <- color_table2()$color
     ggplot(
-      data,
+      data2,
       aes(x = x, y = y)) + 
       geom_point(aes(col=color)) +
       scale_color_manual(values = color_table_mapping) +
@@ -172,7 +193,7 @@ function(input, output) {
       ggtitle("Chosen region") 
   })
   output$info_z <- renderPrint({
-    nearPoints(safe.read(input$tcr_file, input$select_data, input$select_method),
+    nearPoints(docu(),
                input$plot_z_click,
                xvar = "x",
                yvar = "y",
@@ -196,7 +217,7 @@ function(input, output) {
       paste("TCR_analysis-", Sys.Date(), ".csv", sep = "")
     },
     content = function(file2) {
-      write.csv(safe.read(input$tcr_file, input$select_data, input$select_method), 
+      write.csv(docu(), 
                 file2, row.names=FALSE)
     },
     contentType = "text/csv"
